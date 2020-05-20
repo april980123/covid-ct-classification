22e507e0a2cdbd2f04e8252ef4068fe697247dbf
April Guo <Aprilguo@bitbucket.org>
2020-05-20 15:18:49 -0700

    added .mat data files

--- script.m
+++ script.m
@@ -1,10 +1,10 @@
 %%
-covid1 = imread('CT_COVID/2019-novel-Coronavirus-severe-adult-respiratory-dist_2020_International-Jour-p3-89%0.png');
+covid1 = imread('CT_COVID/2020.03.22.20040782-p25-1542.png');
 size(covid1)
 
 %%
 
-fid = fopen('trainCT_COVID.txt');
+fid = fopen('testCT_COVID.txt');
 train_fname = textscan(fid,'%s');
 fclose(fid);
 train_name  = string(train_fname{:});
@@ -13,8 +13,69 @@ train_name
 size(train_name)
 
 %%
-covid_train_data = covid1 ;
-for i = 1:191
-    temp = imread('CT_COVID/'+train_name(i));
-    size(temp)
+first_pic = covid1 ;
+covid1 = rgb2gray(covid1);
+s = size(covid1);
+%the largest dimension of the nonCOVID train pictures are 830 and 797
+for i = s(1):1225
+    for j = s(2):1671
+            covid1(i,j) = 0;
+    end
 end
+
+%%
+test_COVID=covid1;
+test_COVID(1,400)
+size(covid1)
+
+%%
+for i = 2:98
+    temp = imread('CT_COVID/'+train_name(i));
+    if numel(size(temp))>=3
+        temp = rgb2gray(temp);
+    end
+    t = size(temp);
+    if t(1) < 1225 && t(2) < 1671
+        for z = t(1):1225
+            for j = t(2):1671
+                temp(z,j) = 0;
+            end
+        end
+        for j = t(2):1671
+            for z = t(1):1225
+                temp(z,j) = 0;
+            end
+        end
+    end
+    if t(2) < 1671 && t(1) == 1225
+        for j = t(2):1671
+                temp(:,j) = 0;
+        end  
+    end
+    if t(1) < 1225 && t(2) ==1671
+        for z = t(1):1225
+                temp(z,:) = 0;
+        end  
+    end
+%     if t(1) > s(1)
+%         s(1) = t(1);
+%     end
+%     if t(2) > s(2)
+%         s(2) = t(2);
+%     end
+    %size(temp)
+    test_COVID = [test_COVID temp];
+end
+
+
+%%
+save('test_COVID.mat','test_COVID')
+
+%%
+clear variables
+load('train_NonCOVID.mat')
+load('train_COVID.mat')
+load('test_NonCOVID.mat')
+load('test_COVID.mat')
+
+
